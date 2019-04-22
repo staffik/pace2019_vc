0e4baef6c8b678b2a7491bb087b0c56bedd90e27
Gabor Markowski <gabor94@gmail.com>
2019-04-22 14:22:36 +0200

    LP class added

--- solver/main.cpp
+++ solver/main.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 #include "io.h"
 #include "vc.h"
+#include "lp.h"
 
 int main() {
     Graph G = read_input();
 //    preprocess(G, 8);
+    LP_from_graph(G);    
     write_output(5, {2, 4});
     return 0;
 }
